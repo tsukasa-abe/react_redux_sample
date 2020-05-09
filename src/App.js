444ed50e097259279376e2026a59644f3cb42c16
阿部司 <destaque1216@gmail.com>
2020-05-09 19:30:15 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  return(
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("ホゲ太郎です！")}} />
+    </React.Fragment>
+  )
 }
 
 export default App;
